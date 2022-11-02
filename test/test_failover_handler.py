e10f5a9c6408510778bbb07e31072b77a25c64ad
entest <hai@entest.io>
2022-11-02 08:38:49 +0000

    update time

--- test/test_failover_handler.py
+++ test/test_failover_handler.py
@@ -2,12 +2,9 @@
 haimtran basic rds db connect
 02/11/2022
 """
-import datetime
+import time
+from datetime import datetime
 import json
-import uuid
-import logging as logger
-from concurrent.futures import ThreadPoolExecutor
-from threading import current_thread
 import mysql.connector
 import boto3
 
@@ -15,8 +12,6 @@ import boto3
 REGION = "ap-northeast-1"
 # secrete manager
 SECRET_ID = "rds-secrete-name"
-# set log level
-logger.basicConfig(level=logger.DEBUG)
 # parameter for thread
 NUM_ROW = 10000
 CHUNK_SIZE = 100
@@ -66,96 +61,24 @@ def get_connect():
             password=credentials["password"],
             database=credentials["dbname"],
         )
-        print("connected db: {0}".format(conn))
+        print("connected db: {0} at {1}".format(conn, datetime.now().strftime("%D, %H:%M:%S")))
     except:
         print("error connect to db")
     return conn
 
 
-def create_table() -> None:
+def check_db_connect():
     """
-    create a rds table
+    keep pooling the db connection
     """
-    # get connection
-    conn = get_connect()
-    # cursor
-    cur = conn.cursor()
-    # drop table if exists
-    drop = "DROP TABLE IF EXISTS employees"
-    cur.execute(drop)
-    # create table
-    employee_table = (
-        "CREATE TABLE employees ("
-        "    id VARCHAR(36) UNIQUE, "
-        "    name VARCHAR(200) DEFAULT '' NOT NULL, "
-        "    age INT, "
-        "    time TEXT, "
-        "PRIMARY KEY (id))"
-    )
-    cur.execute(employee_table)
-    cur.close()
-    conn.close()
+    while(True):
+        get_connect()
+        time.sleep(3)
 
-
-def fetch_data():
-    """
-    fetch data
-    """
-    # get connection
-    conn = get_connect()
-    # output
-    items = []
-    # cursor connector
-    cur = conn.cursor()
-    #
-    stmt_select = f"SELECT * FROM {TABLE_NAME} LIMIT 100"
-    cur.execute(stmt_select)
-    # parse
-    for row in cur.fetchall():
-        items.append(row)
-        print(row)
-    # close connection
-    cur.close()
-    conn.close()
-    # return
-    return items
-
-
-def write_to_table():
-    """
-    write random data to table
-    """
-    # get connection
-    conn = get_connect()
-    conn.autocommit = True
-    print(f"thread {current_thread().name} connect {conn}")
-    cursor = conn.cursor()
-    print(f"thread {current_thread().name} cursor {cursor}")
-    for k in range(NUM_ROW):
-        print(f"{current_thread().name} insert item {k}")
-        stmt_insert = (
-            "INSERT INTO employees (id, name, age, time) VALUES (%s, %s, %s, %s)"
-        )
-        cursor.execute(
-            stmt_insert,
-            (
-                str(uuid.uuid4()),
-                f"{str(uuid.uuid4())}-{str(uuid.uuid4())}",
-                30,
-                datetime.datetime.now().strftime("%Y-%M-%D-%H-%M-%S"),
-            ),
-        )
-        if k % CHUNK_SIZE == 0:
-            print(f"{current_thread().name} commit chunk {k // CHUNK_SIZE}")
-            conn.commit()
-    # close connection
-    cursor.close()
-    conn.close()
 
 
 if __name__ == "__main__":
     get_db_credentials_from_config()
     # get_db_credentials_from_sm()
-    get_connect()
-    # create_table()
-    # fetch_data()
+    # get_connect()
+    check_db_connect()
